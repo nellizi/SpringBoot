3828ac0ed12fbd674a6607dbdf6eca1c4c616fa7
nellizi <masterliz9095@gmail.com>
2022-10-11 15:41:11 +0900

    서비스 클래스 생성

---
+++ src/main/java/hellospring/hellospring/service/MemberService.java
@@ -0,0 +1,32 @@
+package hellospring.hellospring.service;
+
+import hellospring.hellospring.domain.Member;
+import hellospring.hellospring.repository.MemberRepository;
+import hellospring.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원은 안된다.
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m-> {    //ifPresent = null 이 아니면!
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
