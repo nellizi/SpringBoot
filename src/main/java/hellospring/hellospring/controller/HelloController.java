f3f3a4bb681669472bcd08ba45faea386a625bc1
nellizi <masterliz9095@gmail.com>
2022-10-11 14:20:04 +0900

    MVC와 템플릿 엔진

--- src/main/java/hellospring/hellospring/controller/HelloController.java
+++ src/main/java/hellospring/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hellospring.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,4 +12,10 @@ public class HelloController {
         model.addAttribute("data","helld!!");
         return "hello";
     }
+    @GetMapping("hello-mvc")  // :8080/hello-mvc?name=lizi  name값을 넣어줘야 함
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
